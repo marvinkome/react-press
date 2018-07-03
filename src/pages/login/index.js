1ed1a208b4f28956a94ee5f2b5791ee50b76a535
marvinkome <marvinkome@gmail.com>
2018-07-03 21:28:38 +0100

    prevent loggedIn user from accessing login/signup page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import Router from 'next/router';
 import View from './view';
+import { createToast, isLoggedIn } from '../../lib/helpers';
 import './style.less';
 
 export class Login extends React.Component {
+    static async getInitialProps({ res, isServer, req }) {
+        if (isServer) {
+            if (isLoggedIn(req)) {
+                const backURL = req.header('Referer') || '/';
+                res.writeHead(302, {
+                    Location: backURL
+                });
+                res.end();
+                res.finished = true;
+            }
+        } else {
+            if (isLoggedIn()) {
+                Router.back();
+                createToast('You\'re already logged in');
+            }
+        }
+
+        return {};
+    }
     render() {
         return <View />;
     }
